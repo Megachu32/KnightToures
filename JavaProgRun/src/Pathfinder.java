d41cca23987b198fab325ed2cc018cbdd2f1eb0b
ACE <asaexcelelgib@gmail.com>
2025-06-10 09:31:09 +0700

    more comment

--- JavaProgRun/src/Pathfinder.java
+++ JavaProgRun/src/Pathfinder.java
@@ -13,22 +13,23 @@ public class Pathfinder {
         visited = new boolean[rows][cols];
         return dfs(player, map, panel);
     }
-
+    // deep fith first search method
     private static boolean dfs(PlayerState player, Tile[][] map, MapPanel panel) {
         int x = player.x, y = player.y;
 
+        // check if the current position is out of bounds, blocked, or already visited
         if (outOfBounds(x, y, map) || map[x][y].isBlocked || visited[x][y]) {
             return false;
         }
-
+        // if player reaxhes the exit tile
         if (map[x][y].isExit) {
             panel.repaint();
             return true;
         }
 
-        visited[x][y] = true;
+        visited[x][y] = true;// mark current position as visited
 
-        panel.repaint();
+        panel.repaint(); // repaint to show current position
         sleep(1000); // delay to visualize movement
 
         for (int[] move : knightMoves) {
@@ -45,17 +46,18 @@ public class Pathfinder {
             }
         }
 
-        visited[x][y] = false;
-        panel.repaint();
-        return false;
+        visited[x][y] = false; // backtrack
+        panel.repaint();// calling repaint to show backtracking
+        return false; // no path found
     }
 
+    // sleep method
     private static void sleep(int ms) {
         try {
             Thread.sleep(ms);
         } catch (InterruptedException ignored) {}
     }
-
+    // bound fot the map
     private static boolean outOfBounds(int x, int y, Tile[][] map) {
         return x < 0 || y < 0 || x >= map.length || y >= map[0].length;
     }
